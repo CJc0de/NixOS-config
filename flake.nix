ca274daa97a119f6b2d7202c61e5ebf0d18e6d09
Christopher Johnson <48295654+CJc0de@users.noreply.github.com>
2025-08-30 14:59:48 +0100

    started home-manager config

--- flake.nix
+++ flake.nix
@@ -1,21 +1,32 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
+    home-manager,
     ...
   } @ inputs: let
+    homeManagerModule = [
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.cjcode = ./home;
+      }
+    ];
     mkSystem = extraModules:
       nixpkgs.lib.nixosSystem {
-        pkgs = import inputs.nixpkgs {
+        pkgs = import nixpkgs {
           system = "x86_64-linux";
           config.allowUnfree = true;
         };
         system = "x86_64-linux";
-        modules = extraModules;
+        modules = extraModules ++ homeManagerModule;
       };
   in {
     nixosConfigurations = {
