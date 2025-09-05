8999c62b3a4aee2df43f98f75c60d4102a408413
Christopher Johnson <git@cjcode.uk>
2025-09-05 23:03:49 +0100

    added plasma pannel config

--- home/cjcode.nix
+++ home/cjcode.nix
@@ -44,6 +44,19 @@
   programs.plasma = {
     enable = true;
     overrideConfig = true;
+    panels = [
+      {
+        widgets = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.pager"
+          "org.kde.plasma.taskmanager"
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+          "org.kde.plasma.showdesktop"
+        ];
+      }
+    ];
     fonts = {
       fixedWidth.styleStrategy.noSubpixelAntialias = true;
       fixedWidth.pointSize = 10;
