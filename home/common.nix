6f4b931bf287ce0848c6501beb21cf3e816c7c15
Christopher Johnson <48295654+CJc0de@users.noreply.github.com>
2025-09-05 16:49:05 +0100

    added git config and fixed missing semicolons

---
+++ home/common.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.git = {
+    enable = true;
+    userEmail = "git@cjcode.uk";
+    userName = "Christopher Johnson";
+  };
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
