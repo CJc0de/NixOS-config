a9eafe43a4b4560f826ddc6908e8d458e03bfb7d
Christopher Johnson <git@cjcode.uk>
2025-09-05 17:09:17 +0100

    fixed nh flake dir

--- common/default.nix
+++ common/default.nix
@@ -57,7 +57,7 @@ in
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/cjcode/.dotfiles/NixOS-config";
+    flake = "/nixos-config/NixOS-config";
   };
 
   # Enable the KDE Plasma Desktop Environment.
