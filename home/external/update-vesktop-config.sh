2defc8d4088a5c2c7223a7defe519fb68f906de5
Christopher Johnson <git@cjcode.uk>
2025-09-08 17:59:42 +0100

    testing librewolf userChrome.css and global about:config overrides

--- home/external/update-vesktop-config.sh
+++ home/external/update-vesktop-config.sh
@@ -3,3 +3,5 @@
 sudo -u cjcode cat /home/cjcode/.config/vesktop/settings.json | tee ./vesktop-settings.json >/dev/null
 sudo -u cjcode cat /home/cjcode/.config/vesktop/settings/settings.json | tee ./vencord-settings.json >/dev/null
 
+sudo -u cjcode cat /home/cjcode/.librewolf/v7c18w18.default/chrome/userChrome.css | tee ./librewolf-userChrome.css >/dev/null
+
